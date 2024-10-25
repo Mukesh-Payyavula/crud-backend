ab6e270ba7a293b8942e1f31741533d790bedfeb
Mukesh-Payyavula <payyavulamukesh@gmail.com>
2024-10-25 13:51:12 +0530

    Update Database.js

--- Database/Database.js
+++ Database/Database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/crud");
+        await mongoose.connect("mongodb+srv://payyavulamukesh:Pmy%4052141@crud.j5qda.mongodb.net/crud?retryWrites=true&w=majority");
         console.log("MongoDB connected Successfully");
         
     }catch (error){
